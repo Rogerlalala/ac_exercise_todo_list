0cff235fb658a7c30e5d1d297cfdb6efd4c18107
Roger Cheng <roger801206@gmail.com>
2021-01-16 12:50:49 +0800

    feat: add user login page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const todos = require('./modules/todos')
 const router = express.Router()
+const users = require('./modules/users')
 
 //準備引入路由模組
 const home = require('./modules/home')
@@ -10,6 +11,8 @@ router.use('/', home)
 
 // 將網址結構符合 /todos 字串開頭的 request 導向 todos 模組 
 router.use('/todos', todos)
+
+router.use('/users', users)
 
 //匯出路由器
 module.exports = router
