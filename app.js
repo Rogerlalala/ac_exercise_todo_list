ad147bdcbc74d57496b1968b0012dcc54cab6215
Roger Cheng <roger801206@gmail.com>
2021-04-10 21:17:19 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -27,20 +27,6 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // RESTFUL 路由重構使用 method-override
 app.use(methodOverride('_method'))
 
-// 資料庫 mongoDB
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true })
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
-
 //將 request 導入路由器
 app.use(routes)
 
